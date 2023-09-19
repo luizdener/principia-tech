8e2aa37d1c59bb709508d8c93b2d2b63798ad43e
Luiz Dener <ldenerjesus@gmail.com>
2023-09-19 17:41:21 -0300

    Update Servicos.jsx

--- src/components/servicos/Servicos.jsx
+++ src/components/servicos/Servicos.jsx
@@ -11,7 +11,7 @@ const Servicos = () => {
                 <p>O projeto é importe para garantir uma melhor distribuição de energia e consequentemente prover menor consumo, além de reduzir o risco de falhas que coloquem os residentes em risco!</p>
 
                 <h2><strong>Automações</strong> residenciais</h2>
-                <p>Para você que deseja ter mais conforto e a praticidade na sua residência, o processo de autamação residencial permite que equipamentos e iluminações sejam controlados por meio de aplicativos em seu smartphone ou até mesmo através de gestos. </p>
+                <p>Para você que deseja ter mais conforto e a praticidade na sua residência, o processo de automação residencial permite que equipamentos e iluminações sejam controlados por meio de aplicativos em seu smartphone ou até mesmo através de gestos. </p>
 
                 <h2>Manutenção <strong>elétrica</strong></h2>
                 <p>Realizamos serviços de manutenção preventiva e corretiva em instalações e equipamentos, reduzindo ou sanando riscos de falhas e garantindo mais segurnaça a nossos clientes!</p>
